67f6c90f5e2f8d874ae16022f9291d03e017075a
Yuri Gorshenin <ygorshenin@chromium.org>
2013-09-06 18:45:18 +0400

    Added hot key to the google chrome.

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -33,6 +33,7 @@ main = do
                          } `additionalKeys`
     [ ((controlMask, xK_Shift_L), switchLayout)
     , ((mod4Mask .|. shiftMask, xK_l), spawn "xscreensaver-command -lock")
+    , ((mod4Mask .|. shiftMask, xK_g), spawn "google-chrome")
     , ((0, 0x1008ff11), switchVolume Lower)
     , ((0, 0x1008ff13), switchVolume Raise)
     , ((0, 0x1008ff12), switchVolume Toggle)
