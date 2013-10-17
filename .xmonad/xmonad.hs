2672a420e6b831c9b3fd6d3b26d488ca7dd37aeb
Yuri Gorshenin <ygorshenin@chromium.org>
2013-10-17 14:20:56 +0400

    Added shortcut for palimpsest.

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -34,6 +34,7 @@ main = do
     [ ((controlMask, xK_Shift_L), switchLayout)
     , ((mod4Mask .|. shiftMask, xK_l), spawn "xscreensaver-command -lock")
     , ((mod4Mask .|. shiftMask, xK_g), spawn "google-chrome")
+    , ((mod4Mask .|. shiftMask, xK_p), spawn "palimpsest")
     , ((0, 0x1008ff11), switchVolume Lower)
     , ((0, 0x1008ff13), switchVolume Raise)
     , ((0, 0x1008ff12), switchVolume Toggle)
